d6d72731372e5564db26e77d17d4c600af3fbf7f
hitesh <hiteshjee2400@gmail.com>
2026-02-26 13:43:22 +0530

    new

---
+++ script.js
@@ -0,0 +1,17 @@
+//data types in javascript
+//primitive data types
+let name="hitesh";
+console.log(typeof name);
+let age=19;
+console.log(typeof age);
+let isstudent = true;
+console.log(typeof isstudent);
+let score;
+console.log(typeof score);
+//non primitive user defined
+//object creation
+let car=null;
+console.log(typeof car);
+let person={name:"hitesh kumar",age:19};
+console.log(typeof person);
+console.log(person);
