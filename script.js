cef2aa33626ff9975e448ea8cf79c0fae86e43a3
hitesh <hiteshjee2400@gmail.com>
2026-02-26 14:31:49 +0530

    newe

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//data types in javascript
+/*//data types in javascript
 //primitive data types
 let name="hitesh";
 console.log(typeof name);
@@ -14,4 +14,51 @@ let car=null;
 console.log(typeof car);
 let person={name:"hitesh kumar",age:19};
 console.log(typeof person);
-console.log(person);
+console.log(person);*/
+//arithmetic operator
+/*let a=10;
+let b=5;
+console.log(a+b);//15
+console.log(a-b);//5
+console.log(a*b);//50
+console.log(a/b);//2
+console.log(a%b);//0
+console.log(++a);//11
+console.log(a++);//11
+console.log(--a);//11
+console.log(a--);//11*/
+/*//assignment
+let a=10;
+console.log(a+=3);
+console.log(a-=3);
+console.log(a*=3);
+console.log(a/=3);*/
+//comparison
+/*let a=45;
+let b="45";
+console.log(a==b);//only value checking
+console.log(a===b);//data type checking
+console.log(a!=b);//data type
+console.log(a!==b);//value
+console.log(a>b);
+console.log(a<b);*/
+/*//string operator
+let a="hitesh";
+let b=" kumar";
+console.log(a+b);*/
+/*//ternary opereator
+let age=19;
+let status =(age>18)?"adult":"minor";
+console.log(status);*/
+//condition
+let age=10;
+if(age>18){
+    console.log("drive");
+}
+else if(age>=50){
+    console.log("not drive");
+}
+else{
+    console.log("no valid drive");
+}
+
